74473b67908bc4978aca27d61630352433bdff34
Veer Gosai <71587878+VeerGosai@users.noreply.github.com>
2024-11-19 22:14:25 +0200

    Code

--- scrape.js
+++ scrape.js
@@ -12,9 +12,7 @@ const fs = require('fs');
         const $ = cheerio.load(html);
 
         // Extract the destination data
-        const destination = $(
-            '#flightPageTourStep1 > div.flightPageSummaryAirports.flightPageAviatorView > div.flightPageSummaryDestination > span.flightPageSummaryAirportCode > span > a'
-        ).text();
+        const destination = $('.destinationCity').text().trim();
 
         // Save the destination to a JSON file
         const data = { destination, lastUpdated: new Date().toISOString() };
